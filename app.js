2be1479b5650e924d957640996f1e89690a817e9
Dylan Lott <lott.dylan@gmail.com>
2016-04-27 23:49:37 -0600

    working on getting auth working

---
+++ app.js
@@ -0,0 +1,98 @@
+//STUDIOKEEPER
+//REQUIRES
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var bson = require('bson');
+var app = express();
+var router = express.Router();
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+app.use(cookieParser());
+app.use(session({
+  secret: '1d5adg36s5vf2adr7vwefgv1e46b634',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Controllers 
+var UserCtrl = require('./controllers/UserCtrl');
+
+//Models
+var User = require('./models/User');
+
+//Database
+var mongoUri = "mongodb://localhost:27017/builtright";
+mongoose.connect(mongoUri);
+mongoose.connection.once('open', function() {
+  console.log("Connected to db at " + mongoUri);
+});
+
+//Routes 
+app.use('/users', require('./routes/UserRoutes'));
+
+//Port
+var port = 8080;
+app.listen(process.env.EXPRESS_PORT || port, function() {
+  console.log("The Wolverine Pack is hunting on port ", port);
+});
+
+//Local Login
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, function(username, password, done) {
+  console.log(username, password)
+  User.findOne({ email: username }).exec().then(function(user) {
+    if (!user) {
+      return done(null, false);
+      console.log('no user');
+    }
+    user.comparePassword(password).then(function(isMatch) {
+      if (!isMatch) {
+        console.log('no match');
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  });
+}));
+
+//Authorization
+var requireAuth = function(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.status(403).send({ message: "Logged In" }).end();
+  }
+  return next();
+}
+
+//Deserializer
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+/* Endpoints 
+ **********************************************************************/
+//Auth
+app.post('/users', UserCtrl.createUser);
+app.post('/users/auth', passport.authenticate('local'), function(req, res) {
+  console.log("Logged In");
+  return res.status(200).json(req.user).end();
+});
+app.get('/user', UserCtrl.getUser);
---
+++ controllers/UserCtrl.js
@@ -0,0 +1,48 @@
+var User = require('../models/User.js'); 
+
+module.exports = {
+
+  createUser: function(req, res) {
+    console.log("create users api hit"); 
+    User.findOne({ email: req.body.email })
+      .exec()
+      .then(function(user) {
+        //if we found a user, it's a duplicate
+        if (user) {
+          return res.status(400).json({message: "User with this email already exists."});
+        }
+        //if the user's password is too short ...
+        if (req.body.password.length <= 4) {
+          return res.status(400).json({message: "Your password must be longer than 4 characters."});
+        }
+        //otherwise, create the user
+        var user = new User(req.body);
+        console.log("creating new user"); 
+        user.save(function(err, new_user) {
+          if (err) {
+            console.log("can't create user", err);
+          }
+          res.json(new_user);
+        })
+    })
+  },
+
+  getAllUsers: function(req, res){
+    console.log("get all users activated"); 
+    User.find({}, function(err, users){
+      if(err){
+        console.log("Error getting users. ", err); 
+        return res.status(500).end(); 
+      }else{
+        res.status(200).json(users); 
+      }
+
+    })
+  },
+
+  getUser: function(req, res){
+    console.log("getUser activated"); 
+    return req.user; 
+  }
+
+}
