a56406cc142bddc80efa167bc17f44787c70f50a
Dylan Lott <lott.dylan@gmail.com>
2016-06-13 22:28:24 -0600

    dammit

--- app.js
+++ app.js
@@ -27,76 +27,76 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Controllers 
-var UserCtrl = require('./controllers/UserCtrl');
+// //Controllers 
+// var UserCtrl = require('./controllers/UserCtrl');
 
-//Routes 
-app.use('/users', require('./routes/UserRoutes'));
+// //Routes 
+// app.use('/users', require('./routes/UserRoutes'));
 
-//Models
-var User = require('./models/User');
+// //Models
+// var User = require('./models/User');
 
-//Database
-// var mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/rubylens";
-// mongoose.connect(mongoUri);
-// mongoose.connection.once('open', function() {
-//   console.log("Connected to db at " + mongoUri);
+// //Database
+// // var mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/rubylens";
+// // mongoose.connect(mongoUri);
+// // mongoose.connection.once('open', function() {
+// //   console.log("Connected to db at " + mongoUri);
+// // });
+
+// //Local Login
+// passport.use(new LocalStrategy({
+//   usernameField: 'username',
+//   passwordField: 'password'
+// }, function(username, password, done) {
+//   console.log(username, password)
+//   User.findOne({ email: username }).exec().then(function(user) {
+//     if (!user) {
+//       return done(null, false);
+//       console.log('no user');
+//     }
+//     user.comparePassword(password).then(function(isMatch) {
+//       if (!isMatch) {
+//         console.log('no match');
+//         return done(null, false);
+//       }
+//       return done(null, user);
+//     });
+//   });
+// }));
+
+// //Authorization
+// var requireAuth = function(req, res, next) {
+//   if (!req.isAuthenticated()) {
+//     return res.status(403).send({ message: "Not logged in." }).end();
+//   }
+//   return next();
+// }
+
+// //Deserializer
+// passport.serializeUser(function(user, done) {
+//   done(null, user._id);
 // });
 
-//Local Login
-passport.use(new LocalStrategy({
-  usernameField: 'username',
-  passwordField: 'password'
-}, function(username, password, done) {
-  console.log(username, password)
-  User.findOne({ email: username }).exec().then(function(user) {
-    if (!user) {
-      return done(null, false);
-      console.log('no user');
-    }
-    user.comparePassword(password).then(function(isMatch) {
-      if (!isMatch) {
-        console.log('no match');
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-  });
-}));
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
 
-//Authorization
-var requireAuth = function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return res.status(403).send({ message: "Not logged in." }).end();
-  }
-  return next();
-}
+// /* Endpoints 
+//  **********************************************************************/
+// //Auth
+// app.get('/user', UserCtrl.getUser);
+// app.post('/users', UserCtrl.createUser);
+// app.post('/users/auth', passport.authenticate('local'), function(req, res) {
+//   console.log("Logged In");
+//   return res.status(200).json(req.user).end();
+// });
 
-//Deserializer
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-/* Endpoints 
- **********************************************************************/
-//Auth
-app.get('/user', UserCtrl.getUser);
-app.post('/users', UserCtrl.createUser);
-app.post('/users/auth', passport.authenticate('local'), function(req, res) {
-  console.log("Logged In");
-  return res.status(200).json(req.user).end();
-});
-
-app.get('/logout', function(req, res){
-  req.logout(); 
-  res.status(200).redirect('/'); 
-})
+// app.get('/logout', function(req, res){
+//   req.logout(); 
+//   res.status(200).redirect('/'); 
+// })
 
 //Port
 var port = 4000;
